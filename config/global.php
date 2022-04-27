a6ad9dd43e6b8e54ebe336e723a7d4f3a50764da
mariana20221 <mary_ann_20@hotmail.com>
2022-04-26 22:18:21 -0500

    modificacion de conexion

--- config/global.php
+++ config/global.php
@@ -3,14 +3,14 @@
 define("DB_HOST", "localhost");
 
 // nombre de la base de datos
-define("DB_NAME", "dbsistema");
+define("DB_NAME", "basekiterio");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "miguel");
+define("DB_USERNAME", "kiterio");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "12345678");
+define("DB_PASSWORD", "0601eM_*123Gu");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
